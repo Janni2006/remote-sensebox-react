536b7dcab988af00cee21719d43fc845432b81a4
Jan-Niklas Wielpuetz <janni.wielpuetz@gmail.com>
2021-04-15 08:27:38 +0100

    Added json-server into index.js

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ var cookieParser = require('cookie-parser');
 const app = express();
 const cors = require('cors');
 const axios = require('axios').default;
-const startJobs = require('./handlers/startHandler')
-const apiRouter = require('./api/api')
-var cookieSession = require('cookie-session')
+const startJobs = require('./handlers/startHandler');
+const apiRouter = require('./api/api');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
 require('dotenv/config')
 
@@ -36,6 +39,13 @@ app.get('/', (req, res) => { res.sendFile(__dirname + "/index.html") });
 
 startJobs();
 
-app.listen(process.env.PORT || 4000,
+app.listen(process.env.PORT || 4000, '0.0.0.0',
   () => console.log(`Example app listening at http://localhost:${process.env.PORT || 4000}`)
 );
+
+//start json-server
+server.use(middlewares);
+server.use(router);
+server.listen(3000, '127.0.0.1', () => {
+  console.log("JSON Server is running")
+})
