f9faf0306fabceb35fb0f3636134cac44d0aefb0
Jan-Niklas Wielpuetz <janni.wielpuetz@gmail.com>
2021-04-15 11:27:57 +0100

    Added option for dialog field

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -17,78 +17,45 @@ function Queue() {
 
     React.useEffect(() => {
         const timer = setInterval(async function () {
-            var new_queue = [];
-            await fetch(window.location.origin + "/api/queue", {
-                method: 'GET',
+            const response = await fetch(window.location.origin + "/api/queue", {
+                method: "GET",
                 headers: {
-                    'deviceID': localStorage.getItem("deviceID").toString()
+                    deviceID: localStorage.getItem("deviceID").toString(),
                 },
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    var id = 1;
-
-                    for (var c_position = 0; c_position < data.queue.length; c_position++) {
-                        new_queue.push({
-                            id: id,
-                            friendly_name: data.queue[c_position].friendly_name,
-                            private: data.queue[c_position].private,
-                            running: data.queue[c_position].running,
-                            progress: data.queue[c_position].progress
-                        });
-                        id++;
-                    }
-                });
-            setQueue(new_queue)
+            });
+            const data = await response.json();
+            for (var i = 0; i < 5; i++) {
+                console.log(setQueue(data.queue));
+            }
+            console.log(data.queue)
             console.log(queue)
-            console.log(new_queue)
-        }, 1000)
+            console.log(data.queue)
+        }, 1000);
         return () => {
             clearInterval(timer);
         };
     }, []);
-    const update = () => {
-        var new_queue = [];
-        fetch(window.location.origin + "/api/queue", {
-            method: 'GET',
-            headers: {
-                'deviceID': localStorage.getItem("deviceID").toString()
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Processing Data")
-                var id = 1;
-
-                for (var c_position = 0; c_position < data.queue.length; c_position++) {
-                    new_queue.push({
-                        id: id,
-                        friendly_name: data.queue[c_position].friendly_name,
-                        private: data.queue[c_position].private,
-                        running: data.queue[c_position].running,
-                        progress: data.queue[c_position].progress
-                    });
-                    id++;
-                }
-                console.log(data)
-            });
-        setQueue(new_queue)
-        console.log(queue)
+    const update = async () => {
+        console.log(queue[0])
     }
     return (
         <div style={{ width: '300px', borderRadius: '5px', backgroundColor: 'red' }}>
             <Button variant="contained" onClick={update}>Default</Button>
             <List style={{ margin: '10px' }}>
-                {/* {queue.map(queue_item => {
-                    < QueueObject
-                        key={queue_item.id}
-                        friendly_name={queue_item.friendly_name}
-                        private={queue_item.private}
-                        running={queue_item.running}
-                        progress={queue_item.progress}
-                    />
-
-                })} */}
+                {queue.map(queue_item => {
+                    // < QueueObject
+                    //     key={queue_item.id}
+                    //     friendly_name={queue_item.friendly_name}
+                    //     private={queue_item.private}
+                    //     running={queue_item.running}
+                    //     progress={queue_item.progress}
+                    // />
+                    <p key={queue_item.id}>{queue_item.running}</p>
+                })}
+                <p>Hallo</p>
+                {queue.map(test => {
+                    <p>{test.private}</p>
+                })}
             </List>
         </div>
     )
