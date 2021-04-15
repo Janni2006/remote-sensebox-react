c77c1b971664a7f7511b0e0bd088ddbad016085a
Jan-Niklas Wielpuetz <janni.wielpuetz@gmail.com>
2021-04-15 13:13:16 +0100

    added sketches (not functioning yet)

--- server/api/upload.js
+++ server/api/upload.js
@@ -55,6 +55,57 @@ uploadRouter.post("/upload", async function (req, res) {
             .then(res.send('File uploaded!'));
 
     });
+});
+
+uploadRouter.post("/upload/blockly", async function (req, res) {
+    let sketch;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    console.log(req.files);
+
+    sketch = req.files.file;
+    if (!fs.existsSync(path.join(__basedir, '/uploads/', req.headers.deviceid))) {
+        fs.mkdirSync(path.join(__basedir, '/uploads/', req.headers.deviceid));
+    }
+    uploadPath = path.join(__basedir, '/uploads/', req.headers.deviceid, '/') + crypto.randomBytes(4).toString('hex') + ".ino";
+    uploadPath_blockly = path.join(__basedir, '/uploads/', req.headers.deviceid, '/') + crypto.randomBytes(4).toString('hex') + ".ino";
+
+    if (sketch.name.split(".")[sketch.name.split(".").length] != "ino" && sketch.name.split(".")[sketch.name.split(".").length] != "xml") {
+        return res.status(400).send("Invalid data type!")
+    }
+
+    var queuePosition = 1;
+    await axios.get(process.env.JSON_SERVER + '/uploads').then(function (response) {
+        for (const test of response.data) {
+            if (test.queue_position != 0) {
+                queuePosition++;
+            }
+        }
+    });
+
+    sketch.mv(uploadPath, function (err) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+
+        axios.post(process.env.JSON_SERVER + '/uploads', {
+            sketch: uploadPath,
+            blockly_xml: "",
+            queue_position: queuePosition,
+            friendly_name: sketch.name.split(".")[0],
+            user: req.headers.deviceid,
+            updated: Date.now(),
+            uploaded: 0,
+            demo_completed: false
+        })
+            .then(res.send('File uploaded!'));
+
+    });
 })
 
 module.exports = uploadRouter;
--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
 import QueueObject from "./QueueItem";
 import List from '@material-ui/core/List';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import Button from '@material-ui/core/Button';
-
-const initialQueue = [{
-    id: 0,
-    friendly_name: "",
-    private: false,
-    running: false,
-    progress: 0
-}];
 
 function Queue() {
     const [queue, setQueue] = React.useState([]);
@@ -24,23 +15,14 @@ function Queue() {
                 },
             });
             const data = await response.json();
-            for (var i = 0; i < 5; i++) {
-                console.log(setQueue(data.queue));
-            }
-            console.log(data.queue)
-            console.log(queue)
-            console.log(data.queue)
+            setQueue(data.queue);
         }, 1000);
         return () => {
             clearInterval(timer);
         };
     }, []);
-    const update = async () => {
-        console.log(queue[0])
-    }
     return (
         <div style={{ width: '300px', borderRadius: '5px', backgroundColor: 'red' }}>
-            <Button variant="contained" onClick={update}>Default</Button>
             <List style={{ margin: '10px' }}>
                 {queue.map(queue_item => {
                     // < QueueObject
@@ -52,7 +34,6 @@ function Queue() {
                     // />
                     <p key={queue_item.id}>{queue_item.running}</p>
                 })}
-                <p>Hallo</p>
                 {queue.map(test => {
                     <p>{test.private}</p>
                 })}
