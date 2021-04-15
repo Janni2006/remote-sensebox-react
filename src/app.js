c421aaa0dbe6c3e25689e262e8fa14d0b0415a89
Jan-Niklas Wielpuetz <janni.wielpuetz@gmail.com>
2021-04-15 13:13:16 +0100

    added sketches (not functioning yet)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import FileUpload from './fileupload';
-import Queue from './components/Queue_test';
+import Queue from './components/Queue';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Grid from "@material-ui/core/Grid";
