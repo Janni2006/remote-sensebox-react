8ba92a45edc78d8e0e9c2788670293ffb12db941
Jan-Niklas Wielpuetz <janni.wielpuetz@gmail.com>
2021-04-16 17:41:02 +0100

    Removed Backend

--- server/handlers/startHandler.js
+++
@@ -1,9 +0,0 @@
-const deleteHandler = require("./deleteHandler");
-const flashHandler = require("./flashHandler");
-
-function startJobs() {
-    deleteHandler.start();
-    flashHandler.start();
-}
-
-module.exports = startJobs;
