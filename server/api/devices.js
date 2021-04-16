96d030fb678c7953326de2171408953979ea6551
Jan-Niklas Wielpütz <46819589+Janni2006@users.noreply.github.com>
2021-04-16 14:46:54 +0200

    Delete server directory

--- server/api/devices.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const crypto = require('crypto');
-const deviceRouter = express.Router();
-
-deviceRouter.get('/device/register', function (req, res) {
-    res.json({ deviceID: crypto.randomBytes(16).toString('hex') })
-});
-
-module.exports = deviceRouter;
